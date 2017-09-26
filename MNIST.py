39d3552772e7de100e06eafb9ada43a7e1250eb2
Ricards <G00314715@gmit.ie>
2017-09-26 17:36:40 +0100

    Read Labels

--- MNIST.py
+++ MNIST.py
@@ -2,7 +2,7 @@
 #Importing gzip https://docs.python.org/2/library/gzip.html
 import gzip
 
-f = gzip.open('train-images-idx3-ubyte.gz', 'rb')
+#f = gzip.open('train-images-idx3-ubyte.gz', 'rb')
 
 #File_content stores file data
 #file_content = f.read()
@@ -14,11 +14,35 @@ f = gzip.open('train-images-idx3-ubyte.gz', 'rb')
 #print(firstbyte)
 
 #Reading first four bytes
-magicNum = f.read(4)
+#magicNum = f.read(4)
 
 #b'\x00\x00\x08\x03'
 #print(magicNum)
 
 #Binary = 2051
-magicNum = int.from_bytes(magicNum, 'big')
-print("The magic number is:", magicNum)
+#magicNum = int.from_bytes(magicNum, 'big')
+#print("The magic number is:", magicNum)
+
+#Reading labels, passing in file as parameter
+def readLabelsFromFile(filename):
+    #Ref. https://docs.python.org/3/whatsnew/2.6.html#pep-343-the-with-statement
+    with gzip.open(filename, 'rb') as f:
+        magicNum = f.read(4)
+        magicNum = int.from_bytes(magicNum, 'big')
+        print("The magic number is: ", magicNum)
+
+        numLabels = f.read(4)
+        numLabels = int.from_bytes(numLabels, 'big')
+        print("The number of labels is: ", numLabels)
+
+        #Add to list
+        labels = [f.read(1) for i in range(numLabels)]
+        labels = [int.from_bytes(label, 'big') for label in labels]
+    return labels
+
+print("Training set label file:")
+print("------------------------")
+trainLabels = readLabelsFromFile('train-labels-idx1-ubyte.gz')
+print("\nTest set label file:")
+print("------------------------")
+testLabels = readLabelsFromFile('t10k-labels-idx1-ubyte.gz')
